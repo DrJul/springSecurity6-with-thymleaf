e6ba11e18da8a430dbf61320c64a2e4ded255f88
DrJul <zsuiluj@o2.pl>
2023-03-14 23:51:47 +0100

    init

---
+++ src/main/java/com/example/securitywiththymeleaf/SecurityWithThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.example.securitywiththymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityWithThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityWithThymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/securitywiththymeleaf/SecurityWithThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securitywiththymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityWithThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

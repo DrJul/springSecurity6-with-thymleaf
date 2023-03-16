a733f866d145156f6c7c058bbf5700c67645ea36
DrJul <zsuiluj@o2.pl>
2023-03-17 00:08:27 +0100

    added users pages

--- src/main/java/com/example/securitywiththymeleaf/controller/webController/LoginController.java
+++ src/main/java/com/example/securitywiththymeleaf/controller/webController/LoginController.java
@@ -3,12 +3,21 @@ package com.example.securitywiththymeleaf.controller.webController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller("/")
+@Controller
 public class LoginController {
 
-    @GetMapping
+    @GetMapping("/")
     public String getIndexPage(){
         return "index";
     }
 
+    @GetMapping("/adminPage")
+    public String getAdminPage(){
+        return "adminPage";
+    }
+
+    @GetMapping("/userPage")
+    public String getUserPage(){
+        return "userPage";
+    }
 }

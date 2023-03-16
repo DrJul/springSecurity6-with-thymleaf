8128a9552c14bba1d7b335ce3ca07521ecc335df
DrJul <zsuiluj@o2.pl>
2023-03-16 21:34:00 +0100

    adding basic content

--- src/main/java/com/example/securitywiththymeleaf/SecurityWithThymeleafApplication.java
+++ src/main/java/com/example/securitywiththymeleaf/SecurityWithThymeleafApplication.java
@@ -2,8 +2,9 @@ package com.example.securitywiththymeleaf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class SecurityWithThymeleafApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/example/securitywiththymeleaf/entity/User.java
@@ -0,0 +1,17 @@
+package com.example.securitywiththymeleaf.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String email;
+    private String password;
+}
